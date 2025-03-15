7bc8b053ab2a0587f3c2ad15330061074ade668e
AndersStudent <all22@student.aau.dk>
2025-03-15 23:39:34 +0100

    SkudTing Smukke jader

---
+++ Game/Assets/ZombieSounds.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+
+public class ZombieSounds : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public AudioClip[] soundClips;
+
+    void Start()
+    {
+        // Add an AudioSource component to the game object
+        audioSource = gameObject.AddComponent<AudioSource>();
+        // Start the coroutine to play sounds randomly
+        StartCoroutine(PlayRandomSound());
+    }
+
+    IEnumerator PlayRandomSound()
+    {
+        while (true)
+        {
+            // Wait for one minute
+            yield return new WaitForSeconds(Random.Range(20, 60));
+
+            // 50% chance to play a random sound
+            if (Random.value > 0.9f && soundClips.Length > 0)
+            {
+                AudioClip clip = soundClips[Random.Range(0, soundClips.Length)];
+                audioSource.PlayOneShot(clip);
+            }
+        }
+    }
+}
