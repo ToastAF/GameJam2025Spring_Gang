0aff99f7f91af22c113041224fa98f8dc2982958
AndersStudent <all22@student.aau.dk>
2025-03-15 15:38:59 +0100

    Firework and confeti

---
+++ Game/Assets/Firework.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Firework : MonoBehaviour
+{
+    public ParticleSystem explosionEffect;
+    public float moveSpeed = 2f; // Speed at which it moves to the right
+    public float spinSpeed = 100f; // Speed of rotation
+
+    void Start()
+    {
+        
+        Invoke("Explode", 1f); // Explode after 2 seconds
+    }
+
+    void Update()
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.right * 5, ForceMode.VelocityChange);
+    }
+
+    void Explode()
+    {
+        Instantiate(explosionEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject); // Destroy the firework object after exploding
+    }
+}
